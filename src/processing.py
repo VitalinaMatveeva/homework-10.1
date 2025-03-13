e04fcfe23ddb28afc35cd99642dfd22e79781fc0
vitalina <lulucherry@mail.ru>
2025-03-13 17:35:44 +0300

    rework

--- src/processing.py
+++ src/processing.py
@@ -4,13 +4,13 @@ from operator import itemgetter
 def filter_by_state(transaction_data: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функция, которая возвращает список словарей по ключу state"""
     new_list = []
-    for i in transaction_data:
-        if i["state"] == state:
-            new_list.append(i)
+    for transaction in transaction_data:
+        if transaction["state"] == state:
+            new_list.append(transaction)
     return new_list
 
 
-def sort_by_date(transaction_data: list[dict], reverse=True) -> list[dict]:
+def sort_by_date(transaction_data: list[dict], reverse: bool = True) -> list[dict]:
     """Функция, которая сортирует ключ date по возрастанию"""
     sorted_transaction_data = sorted(transaction_data, key=itemgetter("date"), reverse=reverse)
     return sorted_transaction_data
