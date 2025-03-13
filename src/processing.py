b452383f725120243478bd29e864c88814b239aa
vitalina <lulucherry@mail.ru>
2025-03-13 12:50:16 +0300

    add sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,6 @@
+from operator import itemgetter
+
+
 def filter_by_state(transaction_data: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функция, которая возвращает список словарей по ключу state"""
     new_list = []
@@ -5,3 +8,9 @@ def filter_by_state(transaction_data: list[dict], state: str = "EXECUTED") -> li
         if i["state"] == state:
             new_list.append(i)
     return new_list
+
+
+def sort_by_date(transaction_data: list[dict], reverse=True) -> list[dict]:
+    """Функция, которая сортирует ключ date по возрастанию"""
+    sorted_transaction_data = sorted(transaction_data, key=itemgetter("date"), reverse=reverse)
+    return sorted_transaction_data
